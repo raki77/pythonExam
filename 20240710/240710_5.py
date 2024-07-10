1d286f091ff98b62a2f07aaee14554f3a5d6a6ae
raki77 <rakisiss77@gmail.com>
2024-07-10 12:06:51 +0900

    데이터분석 준전문가 자료수집

---
+++ 20240710/240710_5.py
@@ -0,0 +1,14 @@
+
+name = "연탄이"
+animal = "개"
+age = 4
+
+print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "이고, " + str(age) + "살 입니다.")
+print("우리 집 반려동물은 " , animal , "인데, 이름이 " , name , "이고, " , age , "살 입니다.")
+print(type(age))
+
+
+
+
+
+
