742fd0d0540cc2bdd57491fe22f866c1e2fabcaf
raki77 <rakisiss77@gmail.com>
2024-07-11 16:22:32 +0900

    sample dictionary code as python language.

--- 20240711/240711_3.py
+++ 20240711/240711_3.py
@@ -73,6 +73,32 @@ for i in range(len(movi)):
 # print("Tuple : ", movi)
 
 
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup # ver 4
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+rate_info = 0
+
+# 개별 통화 환율 정보 가져오는 함수 만들기
+def get_exchange_rate(currency_code):
+    url = 'https://finance.naver.com/marketindex/exchangeDetail.naver?marketindexCd=FX_' + currency_code + 'KRW'
+        
+    # 웹페이지 요청 및 HTML 내용 가져오기
+    response = requests.get(url, headers=headers) 
+    html_content = response.text
+    
+    # Beautiful Soup을 사용하여 HTML 내용 파싱
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # 국가명 추출 및 공백 제거
+    country = soup.find('h2').get_text().replace(' ', '')
+    # 환율 정보 추출 및 줄바꿈 문자 제거
+    rate_info = soup.find('p', class_='no_today').get_text().replace('\n', '')
+    rate_info = rate_info.replace('원','').replace(',','')
+    return rate_info
+    
+ 
+
 print()
 ice = {"월드콘":700, "메로나":300, "죠스바":250}
 print(ice["월드콘"])
@@ -82,7 +108,7 @@ print(ice.values())
 
 
 
-ipt = int(input("score:"))
+ipt = int(input("score : "))
 score = {81:"A", 61:"B", 41:"C", 21:"D", 0:"E"} 
 
 for key, value in score.items():
@@ -97,18 +123,22 @@ for key, value in score.items():
     elif ipt < 101:
         ipt = 81
     else:
-        print("Out of lange score")   
+        print("Out of lange score") 
+        break  
     if ipt == key :
-        print(f"{value}")
+        print(f"result : {value}")
  
- 
+
+
 #딕셔너리 환율 정보를 구성하고 원을 입력받아 4개의 통화로 계산하세요.
+# currencyDict = { "달러" : 1167, "엔": 1096, "유로" : 1268, "위안" : 171 } 
 currencyDict = {
-    "달러" : 1167,
-    "엔": 1096,
-    "유로" : 1268,
-    "위안" : 171
+    "달러" : float(get_exchange_rate('USD')),
+    "엔": float(get_exchange_rate('JPY')),
+    "유로" : float(get_exchange_rate('EUR')),
+    "위안" : float(get_exchange_rate('CNY'))
 }
+
 resultDict = {}
 ipt2 = int(input("단위변환 원하는 금액을 입력하세요(원화단위):"))
 for key, value in currencyDict.items():
@@ -121,3 +151,24 @@ for key, value in currencyDict.items():
     if key == "위안" :
         resultDict["위안"] = round(ipt2 / value, 2)        
 print(resultDict)
+
+
+
+# 우편번호는 5자리 입니다. 앞의 3자리가 011부터 019 시작되면 서울 입니다.
+# 아래표를 보고 어느 구의 우편 번호 인지 알려주는 프로그램을 작성 하시오.
+# 딕셔너리 명은 pos라는 이름으로 구성해서 작성하시오.
+# 추가설명 : 010 강북구, 011 강북구, 012강북구, 013 도봉구..)
+
+# 사용자는 01035 입력 # print("안녕하세요? 강북구 주민이시군요") inum = "01035"
+inum = input("우편번호를 입력하시오 : ")
+pos = {"01" : ["강북구","강북구","강북구","도봉구","도봉구","도봉구","노원구","노원구","노원구","노원구"]}
+tmpLst = []
+for key, value in pos.items(): 
+    if key == inum[0:2]:
+        tmpLst = value  
+try:
+    print("안녕하세요?", tmpLst[int(inum[2:3])] ,"주민이시군요")
+except:
+    print("Error")
+    
+
