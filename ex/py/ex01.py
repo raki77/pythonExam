7fe851df6e02bcc4505b5d4d10b453e353516514
raki77 <rakisiss77@gmail.com>
2024-07-15 16:10:27 +0900

    aaa

--- ex/py/ex01.py
+++ ex/py/ex01.py
@@ -1,4 +1,8 @@
 print("hello Python")
 print("hello"*3)
+print(type("hello"*3))
+print(b"hello Python")
+print(type(b"hello Python"))
+
 print("혼자 공부하다 모르면 동영상 강의 참고하세요.1!")
 print("혼자 공부하다 모르면 동영상 강의 참고하세요.2!")
