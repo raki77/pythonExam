68963f27329d0be8180e79a399542679e15af3b4
raki77 <rakisiss77@gmail.com>
2024-07-15 15:38:25 +0900

    organize folder and files. first step.

---
+++ 202407/20240715_setting/240715_1.py
@@ -0,0 +1,59 @@
+
+
+# >>> type(5)
+# <class 'int'>
+# >>> type(53.1)
+# <class 'float'>
+# >>> type('c')
+# <class 'str'>
+# >>> type('ccccccccccc')
+# <class 'str'>
+# >>> type(True)
+# <class 'bool'>
+# >>> type(b'ccccccccc')
+# <class 'bytes'>
+# >>>
+
+# data type < "type은 틀이다."
+# integer : 정수
+# float : 실수
+# string : 문자열
+
+
+# ^ : 캐럿
+# * : asterisk(애스터리스크)
+# & : 앰퍼센트
+# _ : 언더스코어
+# >>> 2**0
+# 1
+# >>> 2**1
+# 2
+# >>> 2**2
+# 4
+# >>> 2**3
+# 8
+# >>> 2**4
+# 16
+# >>> 2**5
+# 32
+# >>>
+
+
+# RAM(메모리에 주소값이 있다.) 
+
+# >>> id(x)
+# 140731112874856 
+# # 주소값
+
+
+# 주소가 다르다. 
+# >>> x=3
+# >>> id(x)
+# 140731112874856
+
+# >>> x=4
+# >>> id(x)
+# 140731112874888
+
+# // = assignment : 할당하다.
+# x변수에 address 주소를 할당한다.
---
+++ ex/py/ex01.py
@@ -0,0 +1,4 @@
+print("hello Python")
+print("hello"*3)
+print("혼자 공부하다 모르면 동영상 강의 참고하세요.1!")
+print("혼자 공부하다 모르면 동영상 강의 참고하세요.2!")
