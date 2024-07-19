54c66dd2c5f6ddf30e2d7023bad2b73f44757178
raki77 <rakisiss77@gmail.com>
2024-07-19 16:14:09 +0900

    save

--- 202407/20240719_loop/240719_3_ans04-01.py
+++ 202407/20240719_loop/240719_3_ans04-01.py
@@ -7,6 +7,14 @@
 # 4. [0, 1, 2, 4, 5, 6, 7]
 # 5. [0, 1, 2, 4, 5, 6, 7]
 # 6. []
+
+# list_a.extend(list_a)
+# list_a.append(10)
+# list_a.insert(3, 0)
+# list_a.remove(3) # 값 : 3을제거
+# list_a.pop(3)
+# list_a.clear()
+# print(list_a)
  
 #  다음 반복문 내부에 if 조건문의 조건식을 채워서 
 #  100 이상의 숫자만 출력하게 만들어보세요. 
@@ -23,9 +31,17 @@
 # for i in numbers:
 #     print(i,"는 짝수입니다." if i%2 == 0 else "홀수입니다.")
 
+# print("-"*60)
 # for i in numbers:
 #     print(i, "는", len(str(i)), "자릿수입니다.")
     
+# print("-"*60)
+# for i in numbers:
+#     print("{}는 {} 자리수입니다.".format(i, len(str(i))))
+
+# print("-"*60)
+# for i in numbers:
+#     print(f"{i}는 {len(str(i))} 자리수입니다.")
   
 # 다음 코드의 빈칸을 채워서 실행 결과처럼 출력되도록 완성해 보세요
 # numbers = [1,2,3,4,5,6,7,8,9]
@@ -44,3 +60,28 @@
 #     print(f"i = {i}, j = {j}")
 #     numbers[j] = numbers[j]**2
 # print(numbers)
+
+
+
+
+
+# --------------------------------------------------
+# 수업시간 문제
+# --------------------------------------------------
+# 빈칸을 채워서 실행 결과에 해당하는 프로그램들을 완성하세요
+# numbers = [273,103,5,32,65,9,72,800,99]
+# odds = []
+# result = []
+# sum = 0
+
+# for i in numbers:      
+#     if i%2 != 0:
+#         odds.append(i)    
+#     if i%3 == 0:
+#         result.append(i)
+#         sum += i
+
+# print("홀수:" , odds)
+# print("3의 배수:" ,result) 
+# print("누적합:", sum)
+    
