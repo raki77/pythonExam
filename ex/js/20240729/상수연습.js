1552a866b4506ccae019210f637f8411cde49316
raki77 <rakisiss77@gmail.com>
2024-07-29 21:02:13 +0900

    update javascript

---
+++ ex/js/20240729/상수연습.js
@@ -0,0 +1,18 @@
+// 변수 초기화1
+let num;
+num = 5;
+console.log(num);
+
+
+// 변수 초기화2
+let num2 = 55;
+console.log(num2);
+
+
+// 상수
+const PI = 3.14;
+console.log(PI);
+// PI = 3.14; // ERROR
+// console.log(PI);
+ 
+
