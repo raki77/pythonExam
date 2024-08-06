67a964946ac39c9d2a90193e1bfa788a620e04f3
raki77 <rakisiss77@gmail.com>
2024-08-06 18:52:32 +0900

    save

---
+++ ex/R-studio/240806_표준편차.r
@@ -0,0 +1,9 @@
+
+stu_height <-c(166,168,170,172,174)
+
+m <- mean(stu_height)
+
+var(stu_height)
+
+sd (stu_height)
+
---
+++ ex/js/240806/240806_1_화살표함수.js
@@ -0,0 +1,67 @@
+// () => {}
+// [Function (anonymous)]
+
+
+// > typeof(() => {})
+// 'function'
+
+// > => {}
+// => {}
+
+ 
+
+// > () =>
+// ... {}
+// [Function (anonymous)]
+
+// > () => {}
+// [Function (anonymous)]
+
+
+// > () => { console.log(
+// ... "hi"); }
+// [Function (anonymous)]
+// >
+
+
+
+// > (() => { console.log("hi");} )
+// [Function (anonymous)]
+
+
+// > f1 = () => { console.log("hi");}
+// [Function: f1]
+// > f1()
+// hi
+
+
+
+
+// > f2 = (gift) => { console.log("hi");}
+// [Function: f2]
+// > f2("apple")
+// hi
+
+
+
+// > f2 = (gift) => { console.log(gift);}
+// [Function: f2]
+// > f2("apple")
+// apple
+
+
+
+// >
+// > f3 = () => { return "banana" ; }
+// [Function: f3]
+  
+// > f4 = (gift) => { console.log(gift); return "banana";}
+// [Function: f4]
+// >
+// >
+// > let result2 = f4("strawberry")
+// strawberry
+
+
+
+// >
