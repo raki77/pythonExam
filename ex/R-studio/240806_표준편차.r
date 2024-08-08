ba25afac2e692e29ce03f13190ee5c76118ad4cf
raki77 <rakisiss77@gmail.com>
2024-08-08 16:32:26 +0900

    save

--- ex/R-studio/240806_표준편차.r
+++ ex/R-studio/240806_표준편차.r
@@ -7,3 +7,19 @@ var(stu_height)
 
 sd (stu_height)
 
+# 상관과 회귀
+# observation : 관측치 : 샘플 : object
+df <- read.csv( choose.files(), header = TRUE)
+
+cor(df$adv, df$sales)
+
+
+?lm
+# 광고에 따라 매출액의 변화
+linear_model1 <- lm('sales ~ adv', df)
+linear_model1
+
+summary(linear_model1)
+ 
+
+0.8879209^2
