bc8ef779d349035ffbacb9df7967678385ba2f70
raki77 <rakisiss77@gmail.com>
2024-08-14 13:47:08 +0900

    save

--- ex/R-studio/240814_다중회귀모델.r
+++ ex/R-studio/240814_다중회귀모델.r
@@ -4,8 +4,16 @@ x1 <- c(2,4,6,8)
 x2 <- c(0,4,2,3)
 y <- c(81,93,91,97)
 
-linear_model2 <- lm(y ~ x1+x2)
+# + 는 x1, x2 독립적으로 고려해서 y에 미치는 영향 분석
+linear_model2 <- lm(y ~ x1 + x2)
 linear_model2
 
+# * 는 x1, x2 이 둘의 교호작용 항인 x1:x2 를 모두 포함
+# 교호작용 * , 다중공선성
+linear_model3_1 <- lm(y ~ x1 * x2)
+linear_model3_1
 
-# 교호작용, 다중공선성
+linear_model3_2 <- lm(y ~ x1+x2+x1:x2)
+linear_model3_2
+
+
